db0b0f9db359aa4f9cd8b47f52d7a532a5534f8f
Romeo Barbieto Junior <163235833+romeojuniorb@users.noreply.github.com>
2024-06-20 15:09:21 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Flip from './Flip.js';
+
+class App extends Component {
+  render() {
+    return( <div className="App">
+      <Flip />
+    </div>
+    );
+  }
+}
+
+export default App;
